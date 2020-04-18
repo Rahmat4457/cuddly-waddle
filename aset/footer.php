ac16a55869b4a57db69fb77986c1070b8004b6d9
Rahmat4457 <53205828+Rahmat4457@users.noreply.github.com>
2020-04-18 20:39:08 +0700

    Add files via upload

---
+++ aset/footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html >
+<head>
+
+    <title>Document</title>
+</head>
+<body>
+<link rel="stylesheet" href="http://localhost/siperpus/aset/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://localhost/siperpus/aset/fontawesome/css/all.min.css">
+
+</body>
+</html>
